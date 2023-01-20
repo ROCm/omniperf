1dd1940bf8c38bf19a4af939d29cbfde0c64163d
coleramos425 <colramos@amd.com>
2023-01-20 15:29:23 -0600

    Comply to Python formatting
    Signed-off-by: coleramos425 <colramos@amd.com>

--- src/utils/specs.py
+++ src/utils/specs.py
@@ -32,7 +32,7 @@ from dataclasses import dataclass
 from pathlib import Path as path
 from textwrap import dedent
 
-gpu_list = {"gfx906", "gfx908", "gfx90a","gfx900"}
+gpu_list = {"gfx906", "gfx908", "gfx90a", "gfx900"}
 
 
 @dataclass

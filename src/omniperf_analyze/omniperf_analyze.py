2b429bdd39649f3cd114c5838afee6ba5699e9ea
coleramos425 <colramos@amd.com>
2023-03-06 14:40:20 -0600

    Comply to Python formatting
    Signed-off-by: coleramos425 <colramos@amd.com>

--- src/omniperf_analyze/omniperf_analyze.py
+++ src/omniperf_analyze/omniperf_analyze.py
@@ -235,7 +235,7 @@ def roofline_only(path_to_dir, dev_id, sort_type, mem_level, kernel_names, verbo
         dev_id,  # [Optional] Specify device id to collect roofline info from
         sort_type,  # [Optional] Sort AI by top kernels or dispatches
         mem_level,  # [Optional] Toggle particular level(s) of memory hierarchy
-        kernel_names, # [Optional] Toggle overlay of kernel names in plot
+        kernel_names,  # [Optional] Toggle overlay of kernel names in plot
         True,  # [Optional] Generate a standalone roofline analysis
     )
 

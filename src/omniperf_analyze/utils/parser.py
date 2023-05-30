ce4e8d6e935cae82c6aff56d12f392aa4fe1f4c4
coleramos425 <colramos@amd.com>
2023-05-30 11:00:41 -0500

    Comply to Python formatting
    Signed-off-by: coleramos425 <colramos@amd.com>

--- src/omniperf_analyze/utils/parser.py
+++ src/omniperf_analyze/utils/parser.py
@@ -468,7 +468,7 @@ def eval_metric(dfs, dfs_type, sys_info, soc_spec, raw_pmc_df, debug):
     """
 
     # confirm no illogical counter values
-    if (raw_pmc_df['pmc_perf']['GRBM_GUI_ACTIVE'] == 0).any():
+    if (raw_pmc_df["pmc_perf"]["GRBM_GUI_ACTIVE"] == 0).any():
         print("WARNING: Dectected GRBM_GUI_ACTIVE == 0\nHaulting execution.")
         sys.exit(1)
 

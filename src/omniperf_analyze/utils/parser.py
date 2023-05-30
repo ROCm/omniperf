1b151e23a14295031edd70a3c526d0d1af017e48
coleramos425 <colramos@amd.com>
2023-05-30 13:30:39 -0500

    Skip GRBM_GUI_ACTIVE checker in --roof-only mode
    Signed-off-by: coleramos425 <colramos@amd.com>

--- src/omniperf_analyze/utils/parser.py
+++ src/omniperf_analyze/utils/parser.py
@@ -467,8 +467,9 @@ def eval_metric(dfs, dfs_type, sys_info, soc_spec, raw_pmc_df, debug):
     Execute the expr string for each metric in the df.
     """
 
-    # confirm no illogical counter values
-    if (raw_pmc_df["pmc_perf"]["GRBM_GUI_ACTIVE"] == 0).any():
+    # confirm no illogical counter values (only consider non-roofline runs)
+    roof_only_run = (sys_info.ip_blocks == "roofline")
+    if not roof_only_run and (raw_pmc_df["pmc_perf"]["GRBM_GUI_ACTIVE"] == 0).any():
         print("WARNING: Dectected GRBM_GUI_ACTIVE == 0\nHaulting execution.")
         sys.exit(1)
 

5b3a8d5075adae72dc331ee933d591383e8de056
Nicholas Curtis <nicurtis@amd.com>
2023-06-06 11:01:37 -0400

    Incorporate review comments
    Signed-off-by: Nicholas Curtis <nicurtis@amd.com>

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -53,6 +53,10 @@ extensions = [
 ]
 
 myst_heading_anchors = 2
+# enable replacement of (tm) & friends
+myst_enable_extensions = [
+    "replacements"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

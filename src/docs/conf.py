c03298e367e112bc9ec68f4c6efccb1a145abf72
Nicholas Curtis <nicurtis@amd.com>
2023-06-07 10:23:49 -0400

    Add options to enable latexpdf builds
    Signed-off-by: Nicholas Curtis <nicurtis@amd.com>

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -89,6 +89,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
+# options for latex output
+latex_engine = 'lualatex'
+latex_show_urls = 'footnote'
+
 
 # -- Options for HTML output -------------------------------------------------
 

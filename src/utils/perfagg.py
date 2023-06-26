5e52983bbf800eed0dfd32b4e0a27c3781713e57
coleramos425 <colramos@amd.com>
2023-06-26 15:30:38 -0500

    Extend filtering into timestamps.csv (#80)
    Signed-off-by: coleramos425 <colramos@amd.com>

--- src/utils/perfagg.py
+++ src/utils/perfagg.py
@@ -345,6 +345,15 @@ def perfmon_coalesce(pmc_files_list, workload_dir, soc):
                             # initial counter in this channel
                             pmc_list["TCC2"][str(ch)] = [counter]
 
+
+    # add a timestamp file
+    fd = open(workload_perfmon_dir + "/timestamps.txt", "w")
+    fd.write("pmc:\n\n")
+    fd.write("gpu:\n")
+    fd.write("range:\n")
+    fd.write("kernel:\n")
+    fd.close()
+    
     # sort the per channel counter, so that same counter in all channels can be aligned
     for ch in range(perfmon_config[soc]["TCC_channels"]):
         pmc_list["TCC2"][str(ch)].sort()

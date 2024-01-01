a667f5175af553215e3367fbc30aae3534e2cf93
Fei Zheng <Fei.Zheng@amd.com>
2024-01-01 15:35:49 -0700

    1st round for merging code
    Signed-off-by: Fei Zheng <Fei.Zheng@amd.com>

--- src/omniperf_analyze/utils/file_io.py
+++ src/omniperf_analyze/utils/file_io.py
@@ -123,7 +123,6 @@ def create_df_kernel_top_stats(
     filter_gpu_ids,
     filter_dispatch_ids,
     time_unit,
-    max_kernel_num,
     sortby="sum",
 ):
     """
@@ -183,14 +182,9 @@ def create_df_kernel_top_stats(
     #   Sort by total time as default.
     if sortby == "sum":
         grouped = grouped.sort_values(by=("Sum" + time_unit_str), ascending=False)
-
-        grouped = grouped.head(max_kernel_num)  # Display only the top n results
-
         grouped.to_csv(os.path.join(raw_data_dir, "pmc_kernel_top.csv"), index=False)
     elif sortby == "kernel":
         grouped = grouped.sort_values("Kernel_Name")
-
-        grouped = grouped.head(max_kernel_num)  # Display only the top n results
         grouped.to_csv(os.path.join(raw_data_dir, "pmc_kernel_top.csv"), index=False)
 
 

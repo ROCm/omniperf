1d103de881ffaa161b8d932324c964111af985eb
Jose Santos <josantos@login1.hpcfund>
2024-02-26 13:05:56 -0600

    Add testing save-dfs in analyze mode
    Signed-off-by: Jose Santos <josantos@login1.hpcfund>

--- tests/test_analyze_commands.py
+++ tests/test_analyze_commands.py
@@ -1628,3 +1628,35 @@ def test_dependency_MI100():
         ):
             omniperf.main()
     assert e.value.code == 0
+
+@pytest.mark.misc
+def test_save_dfs():
+    output_path = "tests/workloads/vcopy/saved_analysis"
+    with pytest.raises(SystemExit) as e:
+        with patch(
+            "sys.argv",
+            [
+                "omniperf",
+                "analyze",
+                "--path",
+                "tests/workloads/vcopy/MI100",
+                "--save-dfs",
+                output_path,
+            ],
+        ):
+            omniperf.main()
+    assert e.value.code == 0
+    files_in_workload = os.listdir(output_path)
+    for file_name in files_in_workload:
+        import pandas as pd
+        df = pd.read_csv(output_path + "/" + file_name)
+        print(file_name)
+        single_row_tables = [
+            '0.1_Top_Kernels.csv',
+            '13.3_Instruction_Cache_-_L2_Interface.csv',
+            '18.1_Aggregate_Stats_(All_32_channels).csv'
+        ]
+        if file_name in single_row_tables:
+            assert len(df.index) == 1
+        else:
+            assert len(df.index) >= 3

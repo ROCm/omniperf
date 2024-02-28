0a7ad3bed04e937bac06ed849269471dbc7d6e1e
Nicholas Curtis <nicurtis@amd.com>
2024-02-28 10:57:20 -0500

    fix for improper display of % of peak for instruction mix panel
    Signed-off-by: Nicholas Curtis <nicurtis@amd.com>

--- src/utils/gui.py
+++ src/utils/gui.py
@@ -227,10 +227,13 @@ def build_bar_chart(display_df, table_config, barchart_elements, norm_filt):
                 ).update_xaxes(range=[0, 1638])
             )  # append second GB/s chart
         else:
+            key = 'Avg'
+            if table_config['id'] in [1101]:
+                key = 'Pct of Peak'
             d_figs.append(
                 px.bar(
                     display_df,
-                    x="Avg",
+                    x=key,
                     y="Metric",
                     color="Avg",
                     range_color=[0, 100],

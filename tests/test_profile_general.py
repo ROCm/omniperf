22b4a97ba8dd7d45edbe42df4bfe65f4f2a0d22c
Jose Santos <josantos@amd.com>
2024-03-13 11:02:47 -0500

    Remove stubbed binary
    Signed-off-by: Jose Santos <josantos@amd.com>

--- tests/test_profile_general.py
+++ tests/test_profile_general.py
@@ -18,7 +18,7 @@ import test_utils
 config = {}
 config["omniperf"] = SourceFileLoader("omniperf", "src/omniperf").load_module()
 config["kernel_name_1"] = "vecCopy(double*, double*, double*, int, int) [clone .kd]"
-config["app_1"] = ["./sample/vcopy_MI100", "-n", "1048576", "-b", "256", "-i", "3"]
+config["app_1"] = ["./tests/vcopy", "-n", "1048576", "-b", "256", "-i", "3"]
 config["cleanup"] = True
 config["COUNTER_LOGGING"] = False
 config["METRIC_COMPARE"] = False

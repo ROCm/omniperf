5e7b3b604ecd541fd61c451ae56263c8be809b7e
Peter Jun Park <peter.park@amd.com>
2024-05-06 11:49:08 -0400

    Update docs build config
    Signed-off-by: Peter Jun Park <peter.park@amd.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,87 +1,55 @@
+# MIT License
+
+# Copyright (c) 2023 - 2024 Advanced Micro Devices, Inc. All rights reserved.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import shutil
-import jinja2
-import os
+import re
 
-# Environment to process Jinja templates.
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
+from rocm_docs import ROCmDocs
 
-# Jinja templates to render out.
-templates = []
+with open('../VERSION', encoding='utf-8') as f:
+    match = re.search(r'([0-9.]+)[^0-9.]+', f.read())
+    if not match:
+        raise ValueError("VERSION not found!")
+    version_number = match[1]
+left_nav_title = f"Omniperf {version_number} documentation"
 
-# Render templates and output files without the last extension.
-# For example: 'install.md.jinja' becomes 'install.md'.
-for template in templates:
-    rendered = jinja_env.get_template(template).render()
-    with open(os.path.splitext(template)[0], "w") as file:
-        file.write(rendered)
-
-# shutil.copy2("../RELEASE.md", "./about/release-notes.md")
-# # Keep capitalization due to similar linking on GitHub's markdown preview.
-# shutil.copy2("../CHANGELOG.md", "./about/CHANGELOG.md")
-
-latex_engine = "xelatex"
-latex_elements = {
-    "fontpkg": r"""
-\usepackage{tgtermes}
-\usepackage{tgheros}
-\renewcommand\ttdefault{txtt}
-"""
-}
-
-# configurations for PDF output by Read the Docs
-project = "ROCm Documentation"
+# for PDF output on Read the Docs
+project = "Omniperf documentation"
 author = "Advanced Micro Devices, Inc."
 copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
-version = "6.0.1"
-release = "6.0.1"
-setting_all_article_info = True
-all_article_info_os = ["linux", "windows"]
-all_article_info_author = ""
-
-# pages with specific settings
-# article_pages = [
-#     {"file": "about/release-notes", "os": ["linux", "windows"], "date": "2024-01-31"},
-#     {"file": "about/CHANGELOG", "os": ["linux", "windows"], "date": "2024-01-31"},
-#     {"file": "install/windows/install-quick", "os": ["windows"]},
-#     {"file": "install/linux/install-quick", "os": ["linux"]},
-#     {"file": "install/linux/install", "os": ["linux"]},
-#     {"file": "install/linux/install-options", "os": ["linux"]},
-#     {"file": "install/linux/prerequisites", "os": ["linux"]},
-#     {"file": "install/docker", "os": ["linux"]},
-#     {"file": "install/magma-install", "os": ["linux"]},
-#     {"file": "install/pytorch-install", "os": ["linux"]},
-#     {"file": "install/tensorflow-install", "os": ["linux"]},
-#     {"file": "install/windows/install", "os": ["windows"]},
-#     {"file": "install/windows/prerequisites", "os": ["windows"]},
-#     {"file": "install/windows/cli/index", "os": ["windows"]},
-#     {"file": "install/windows/gui/index", "os": ["windows"]},
-#     {"file": "about/compatibility/docker-image-support-matrix", "os": ["linux"]},
-#     {"file": "about/compatibility/user-kernel-space-compat-matrix", "os": ["linux"]},
-#     {"file": "reference/library-index", "os": ["linux"]},
-#     {"file": "how-to/deep-learning-rocm", "os": ["linux"]},
-#     {"file": "how-to/gpu-enabled-mpi", "os": ["linux"]},
-#     {"file": "how-to/system-debugging", "os": ["linux"]},
-#     {"file": "how-to/tuning-guides", "os": ["linux", "windows"]},
-#     {"file": "rocm-a-z", "os": ["linux", "windows"]},
-# ]
-
-exclude_patterns = ["temp"]
+version = version_number
+release = version_number
 
 external_toc_path = "./sphinx/_toc.yml"
 
-extensions = ["rocm_docs"]
+docs_core = ROCmDocs(left_nav_title)
+docs_core.setup()
 
-external_projects_current_project = "rocm"
+external_projects_current_project = "omniperf"
 
-html_theme = "rocm_docs_theme"
-html_theme_options = {"flavor": "rocm-docs-home"}
-
-html_title = "ROCm Documentation"
-
-html_theme_options = {"link_main_doc": False}
+for sphinx_var in ROCmDocs.SPHINX_VARS:
+    globals()[sphinx_var] = getattr(docs_core, sphinx_var)

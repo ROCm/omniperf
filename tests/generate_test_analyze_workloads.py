db7d77ca3febf2925d6b34c4414b78cf61beac16
JoseSantosAMD <Jose.Santos@amd.com>
2024-05-30 10:24:45 -0500

    updating expected exit codes for pytest
    Signed-off-by: JoseSantosAMD <Jose.Santos@amd.com>

--- tests/generate_test_analyze_workloads.py
+++ tests/generate_test_analyze_workloads.py
@@ -16,31 +16,22 @@ if __name__ == "__main__":
     workloads = glob.glob(workloads_path + "/*")
 
     with open("test_analyze_workloads.py", "a") as f:
-        with open("test_saved_analysis.py", "a") as g:
-            for workload in workloads:
-                workload_name = workload[workload.rfind("/") + 1 :]
-                archs = os.listdir(workload)
-                for arch in archs:
-                    test = (
-                        "\n\ndef test_analyze_"
-                        + workload_name
-                        + "_"+arch+"():"+
-                        "\n\twith pytest.raises(SystemExit) as e:"+
-                        "\n\t\twith patch('sys.argv',['omniperf', 'analyze', '--path', '"
-                        + workload
-                        + "/"+arch+"']):\n\t\t\tomniperf.main()"+
-                        "\n\tassert e.value.code == 0"
-                    )
-                    f.write(test)
-                    test = (
-                        "\n\ndef test_saved_"
-                        + workload_name
-                        + "_"+arch+"():\n\tcompare('"
-                        + workload
-                        + "/"+arch+"/prev_analysis', '"
-                        + "/"+arch+"/prev_analysis', '"
-                        + workload
-                        + "/"+arch+"/saved_analysis')"
-                        + "/"+arch+"/saved_analysis')"
-                    )
-                    g.write(test)
+        for workload in workloads:
+            workload_name = workload[workload.rfind("/") + 1 :]
+            archs = os.listdir(workload)
+            for arch in archs:
+                test = (
+                    "\n\ndef test_analyze_"
+                    + workload_name
+                    + "_"
+                    + arch
+                    + "():"
+                    + "\n\twith pytest.raises(SystemExit) as e:"
+                    + "\n\t\twith patch('sys.argv',['omniperf', 'analyze', '--path', '"
+                    + workload
+                    + "/"
+                    + arch
+                    + "']):\n\t\t\tomniperf.main()"
+                    + "\n\tassert e.value.code == 0"
+                )
+                f.write(test)

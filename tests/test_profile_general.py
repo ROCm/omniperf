6b727c13f8f13f9919cc0c7b0e72346fe1e23627
benrichard-amd <ben.richard@amd.com>
2024-08-03 18:59:40 -0500

    Fix ctests for MI100
    Signed-off-by: xuchen-amd <xuchen@amd.com>

--- tests/test_profile_general.py
+++ tests/test_profile_general.py
@@ -71,22 +71,58 @@ ALL_CSVS = sorted(
         "pmc_perf_6.csv",
         "pmc_perf_7.csv",
         "pmc_perf_8.csv",
+        "pmc_perf_9.csv",
+        "pmc_perf_10.csv",
         "sysinfo.csv",
         "timestamps.csv",
     ]
 )
 ALL_CSVS_MI200 = sorted(
-    ALL_CSVS
-    + [
+    [
+        "SQ_IFETCH_LEVEL.csv",
+        "SQ_INST_LEVEL_LDS.csv",
+        "SQ_INST_LEVEL_SMEM.csv",
+        "SQ_INST_LEVEL_VMEM.csv",
+        "SQ_LEVEL_WAVES.csv",
+        "pmc_perf.csv",
+        "pmc_perf_0.csv",
+        "pmc_perf_1.csv",
+        "pmc_perf_2.csv",
+        "pmc_perf_3.csv",
+        "pmc_perf_4.csv",
+        "pmc_perf_5.csv",
+        "pmc_perf_6.csv",
+        "pmc_perf_7.csv",
+        "pmc_perf_8.csv",
         "pmc_perf_9.csv",
         "roofline.csv",
+        "sysinfo.csv",
+        "timestamps.csv",
     ]
 )
 ALL_CSVS_MI300 = sorted(
-    ALL_CSVS
-    + [
+    [
+        "SQ_IFETCH_LEVEL.csv",
+        "SQ_INST_LEVEL_LDS.csv",
+        "SQ_INST_LEVEL_SMEM.csv",
+        "SQ_INST_LEVEL_VMEM.csv",
+        "SQ_LEVEL_WAVES.csv",
+        "pmc_perf.csv",
+        "pmc_perf_0.csv",
+        "pmc_perf_1.csv",
+        "pmc_perf_2.csv",
+        "pmc_perf_3.csv",
+        "pmc_perf_4.csv",
+        "pmc_perf_5.csv",
+        "pmc_perf_6.csv",
+        "pmc_perf_7.csv",
+        "pmc_perf_8.csv",
+        "sysinfo.csv",
+        "timestamps.csv",
     ]
+
 )
+
 ROOF_ONLY_FILES = sorted(
     [
         "empirRoof_gpu-0_fp32_fp64.pdf",
@@ -859,6 +895,8 @@ def test_block_TCC():
         "pmc_perf_6.csv",
         "pmc_perf_7.csv",
         "pmc_perf_8.csv",
+        "pmc_perf_9.csv",
+        "pmc_perf_10.csv",
         "sysinfo.csv",
         "timestamps.csv",
     ]
@@ -1239,9 +1277,6 @@ def test_block_SQ_SQC_TCP_CPC():
         "pmc_perf_3.csv",
         "pmc_perf_4.csv",
         "pmc_perf_5.csv",
-        "pmc_perf_6.csv",
-        "pmc_perf_7.csv",
-        "pmc_perf_8.csv",
         "sysinfo.csv",
         "timestamps.csv",
     ]
@@ -1314,9 +1349,6 @@ def test_block_SQ_SPI_TA_TCC_CPF():
         "pmc_perf_3.csv",
         "pmc_perf_4.csv",
         "pmc_perf_5.csv",
-        "pmc_perf_6.csv",
-        "pmc_perf_7.csv",
-        "pmc_perf_8.csv",
         "sysinfo.csv",
         "timestamps.csv",
     ]

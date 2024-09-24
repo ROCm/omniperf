72fcc7d2344bcada4c1505cd74c6fd3e0e71ff6e
xuchen-amd <xuchen@amd.com>
2024-09-23 21:40:56 -0400

    Inital commit of the rocprof-compute rebrand process. This commit renames omniperf to rocprof-compute in places highly visible to an average external user, i.e., renaming in commands, usage messages, logs, and other command line outputs.
    Signed-off-by: xuchen-amd <xuchen@amd.com>

--- src/roofline.py
+++ src/roofline.py
@@ -418,7 +418,7 @@ class Roofline:
                 if not self.__args.remaining:
                     console_error(
                         "profiling"
-                        "An <app_cmd> is required to run.\nomniperf profile -n test -- <app_cmd>"
+                        "An <app_cmd> is required to run.\nrocprof-compute profile -n test -- <app_cmd>"
                     )
                 # TODO: Add an equivelent of characterize_app() to run profiling directly out of this module
 

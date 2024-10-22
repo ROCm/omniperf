71a0fa52967947d3cc23478ee29fe7686de1a911
zichguan-amd <zichuan.guan@amd.com>
2024-10-22 17:56:10 -0400

    Add three-way comparison test case
    Signed-off-by: zichguan-amd <zichuan.guan@amd.com>

--- tests/test_analyze_commands.py
+++ tests/test_analyze_commands.py
@@ -1153,6 +1153,52 @@ def test_baseline():
             omniperf.main()
     assert e.value.code == 0
 
+    with pytest.raises(SystemExit) as e:
+        with patch(
+            "sys.argv",
+            [
+                "omniperf",
+                "analyze",
+                "--path",
+                "tests/workloads/multikernel/MI200",
+                "-k",
+                "0",
+                "--path",
+                "tests/workloads/multikernel/MI200",
+                "-k",
+                "1",
+                "--path",
+                "tests/workloads/multikernel/MI200",
+                "-k",
+                "2",
+            ],
+        ):
+            omniperf.main()
+    assert e.value.code == 0
+
+    with pytest.raises(SystemExit) as e:
+        with patch(
+            "sys.argv",
+            [
+                "omniperf",
+                "analyze",
+                "--path",
+                "tests/workloads/multikernel/MI200",
+                "-k",
+                "0",
+                "--path",
+                "tests/workloads/multikernel/MI200",
+                "-k",
+                "1",
+                "--path",
+                "tests/workloads/vcopy/MI100",
+                "-k",
+                "0",
+            ],
+        ):
+            omniperf.main()
+    assert e.value.code == 0
+
 
 @pytest.mark.misc
 def test_dependency_MI100():
